38a89fe2c57ddaf498893cc327bb88c7f24fc067
Austin Simpson <asimpson@chapman.edu>
2022-10-15 11:13:39 -0700

    api works

--- main.py
+++ main.py
@@ -2,14 +2,23 @@
 # 4s0I-9PIRrJJo4-cEcpocA
 # endpoint:
 # https://api.yelp.com/v3
-
-import requests
 from yelp.client import Client
+import requests
 
-MY_API_KEY = HIHXOVLf-jSEn24ROb755mxQYjhp6SAyKBMZseH-KpUV5dehqGJDKyHkO1Ah5U8sYnlz_xMekjLhrWh6eiZSFlWwbGzF0lWIdPuJtjyeRTFX_MY3_bSHRJ4opu5KY3Yx
+API_KEY = 'HIHXOVLf-jSEn24ROb755mxQYjhp6SAyKBMZseH-KpUV5dehqGJDKyHkO1Ah5U8sYnlz_xMekjLhrWh6eiZSFlWwbGzF0lWIdPuJtjyeRTFX_MY3_bSHRJ4opu5KY3Yx'
+API_HOST = 'https://api.yelp.com'
+BUSINESS_PATH = '/v3/businesses/'
 
-client = Client(MY_API_KEY)
+def get_business(business_id):
+    business_path = BUSINESS_PATH + business_id
+    url = API_HOST + business_path + '/reviews'
+    headers = {'Authorization': f"Bearer {API_KEY}"}
 
-example = requests.get('https://api.yelp.com/v3/businesses/search?term=delis')
+    response = requests.get(url, headers=headers)
 
-print(example)
+    return response.json()
+
+
+
+results = get_business('la-taqueria-san-francisco-2')
+print(results)
